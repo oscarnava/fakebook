838d86c3a090fd8cc8ce52e4647f8a5f0fb90e3b
oscarnava <oscarnava@gmail.com>
2019-09-30 17:55:57 -0500

    Prototype of tests (in progress)

--- spec/views/posts/edit.html.erb_spec.rb
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -1,21 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "posts/edit", type: :view do
+RSpec.describe 'posts/edit', type: :view do
+  let(:user) { User.create(username: 'Dummy', email: 'dummy@dummy.com', password: 'abcdefg') }
+
   before(:each) do
-    @post = assign(:post, Post.create!(
-      :content => "MyString",
-      :user => nil
-    ))
+    @post = assign(:post, Post.create!(content: 'MyString', user: user))
   end
 
-  it "renders the edit post form" do
+  it 'renders the edit post form' do
     render
 
-    assert_select "form[action=?][method=?]", post_path(@post), "post" do
-
-      assert_select "input[name=?]", "post[content]"
-
-      assert_select "input[name=?]", "post[user_id]"
+    assert_select 'form[action=?][method=?]', post_path(@post), 'post' do
+      assert_select 'input[name=?]', 'post[content]'
+      assert_select 'input[name=?]', 'post[user_id]'
     end
   end
 end

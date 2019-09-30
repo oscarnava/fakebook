06bf6d6e4fc979962955fa8642e76ff9494ea617
oscarnava <oscarnava@gmail.com>
2019-09-30 17:55:57 -0500

    Prototype of tests (in progress)

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,22 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "posts/index", type: :view do
+RSpec.describe 'posts/index', type: :view do
+  let(:user) { User.create(username: 'Dummy', email: 'dummy@dummy.com', password: 'abcdefg') }
+
   before(:each) do
     assign(:posts, [
-      Post.create!(
-        :content => "Content",
-        :user => nil
-      ),
-      Post.create!(
-        :content => "Content",
-        :user => nil
-      )
-    ])
+             Post.create!(
+               content: 'Content',
+               user: user
+             ),
+             Post.create!(
+               content: 'Content',
+               user: user
+             )
+           ])
   end
 
-  it "renders a list of posts" do
+  it 'renders a list of posts' do
     render
-    assert_select "tr>td", :text => "Content".to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select 'tr>td', text: 'Content', count: 2
+    assert_select 'tr>td:nth-child(2)', text: user.id.to_s, count: 2
   end
 end
--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "posts/show", type: :view do
+RSpec.describe 'posts/show', type: :view do
+  let(:user) { User.create(username: 'Dummy', email: 'dummy@dummy.com', password: 'abcdefg') }
+
   before(:each) do
     @post = assign(:post, Post.create!(
-      :content => "Content",
-      :user => nil
-    ))
+                            content: 'Content',
+                            user: user
+                          ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Content/)
     expect(rendered).to match(//)

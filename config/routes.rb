ce2f3dd8abc10632b98f5fb62ab998a0d591fa00
oscarnava <oscarnava@gmail.com>
2019-10-01 15:40:11 -0500

    Added user profile display

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root "posts#index"
+  root 'posts#index'
 
+  get 'users/:id', to: 'users#show', id: /\d+/, as: 'user'
   resources :posts
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

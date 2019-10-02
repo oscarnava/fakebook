218d246ec8973fa8f27d8ca62f197c55767965b9
Mauricio Robayo <rfmajo@gmail.com>
2019-10-02 11:14:20 -0500

    Remove unneeded helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,8 +7,4 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: 'gravatar')
   end
-
-  def user_path_by_name(user)
-    "#{users_path}/#{user.username}"
-  end
 end

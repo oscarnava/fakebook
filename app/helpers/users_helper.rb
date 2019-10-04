dcfa1671e47407e2e7532f78e28d81ade20207c8
Mauricio Robayo <rfmajo@gmail.com>
2019-10-04 11:59:39 -0500

    Use image from facebook

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 module UsersHelper
-  def gravatar_for(user, size: 80, link_it: true)
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    tag = image_tag(gravatar_url, alt: user.username, class: 'gravatar')
+  def avatar_for(user, size: 80, link_it: true)
+    if user.image.blank?
+      avatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      avatar_url = "https://secure.gravatar.com/avatar/#{avatar_id}?s=#{size}"
+    else
+      avatar_url = user.image
+    end
+    tag = image_tag(avatar_url, alt: user.username, class: 'gravatar', width: size)
     link_it ? link_to(tag, user_path(user.username)) : tag
   end
 end

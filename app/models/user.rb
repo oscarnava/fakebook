3e207889e68ac96ee0c2ff6372798ef0bc163a62
oscarnava <oscarnava@gmail.com>
2019-10-07 12:30:35 -0500

    Rename following & followers assoc.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,30 +10,27 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  has_many :following, foreign_key: 'requester_id', class_name: :Friendship
-  # has_many :friends, through: :friendships, source: :requestee
-
-  has_many :followers, foreign_key: 'requestee_id', class_name: :Friendship
-  # has_many :followers, through: :requests, source: :requester
+  has_many :following_friendships, foreign_key: 'requester_id', class_name: :Friendship
+  has_many :followers_friendships, foreign_key: 'requestee_id', class_name: :Friendship
 
   default_scope { order(:username) }
 
   # Requests made by others to current_user
   def active_followers
-    followers.filter(&:accepted?).map(&:requester).sort_by(&:username)
+    followers_friendships.filter(&:accepted?).map(&:requester).sort_by(&:username)
   end
 
   def pending_followers
-    followers.filter(&:pending?).map(&:requester).sort_by(&:username)
+    followers_friendships.filter(&:pending?).map(&:requester).sort_by(&:username)
   end
 
   # Requests made by current_user to others
   def pending_following
-    following.filter(&:pending?).map(&:requestee).sort_by(&:username)
+    following_friendships.filter(&:pending?).map(&:requestee).sort_by(&:username)
   end
 
   def active_following
-    following.filter(&:accepted?).map(&:requestee).sort_by(&:username)
+    following_friendships.filter(&:accepted?).map(&:requestee).sort_by(&:username)
   end
 
   def rejected_followings

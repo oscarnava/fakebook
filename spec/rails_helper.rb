adcb80922a0c3a77767c2ee62fafe5057157d1a4
oscarnava <oscarnava@gmail.com>
2019-10-08 12:02:52 -0500

    Clean database before tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :deletion
+    # DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean
+  end
 end
